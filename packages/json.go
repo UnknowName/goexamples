63535db7f5bb6e848b5dedb8adac3df1156c86e4
UnknowName <burtte@sina.com>
2020-03-24 12:18:56 +0800

    Add JSON

---
+++ packages/json.go
@@ -0,0 +1,29 @@
+package packages
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Map2Json(){
+	dict := make(map[string]string)
+	dict["name"] = "unknowname"
+	dict["country"] = "china"
+	jsonByte, err := json.Marshal(dict)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(jsonByte))
+}
+
+func Json2Map(){
+	jsonStr := `{"name":"unknowname", "country": "china"}`
+	dict := make(map[string]string)
+	err := json.Unmarshal([]byte(jsonStr), &dict)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dict)
+}
---
+++ packages/json_test.go
@@ -0,0 +1,11 @@
+package packages
+
+import "testing"
+
+func TestMap2Json(t *testing.T) {
+	Map2Json()
+}
+
+func TestJson2Map(t *testing.T) {
+	Json2Map()
+}
